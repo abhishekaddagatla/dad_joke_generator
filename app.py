5a82d76ebbf4d0ff7fdf4508b32adf8235443af1
abhishekaddagatla <abhi.adagatla@gmail.com>
2023-07-11 23:03:17 -0400

    getting frontend to work

--- app.py
+++ app.py
@@ -1,29 +1,18 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request, render_template
 import openai
 import os
-from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
+# load_dotenv()
+openai.api_key = "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"
 
 
 @app.route('/')
 def dad_joke():
-    topic = request.args.get('topic','')
-
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"tell me a dad joke {topic}.",
-        temperature=1,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    joke = response.choices[0].text.strip()
-    return {"joke":joke}
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
