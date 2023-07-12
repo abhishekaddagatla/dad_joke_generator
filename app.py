5766157b9f1409e20a0dbfe0534a314027e02967
abhishekaddagatla <abhi.adagatla@gmail.com>
2023-07-11 23:42:22 -0400

    got ajax working

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 # load_dotenv()
 openai.api_key = "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"
 
+@app.route('/generate_joke', methods=['POST'])
+def generate_joke(query):
+    return "hello world!"
+    #api call to get the joke with query, return the joke in json
 
 @app.route('/')
 def dad_joke():
--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,28 @@
-async function generateJoke(){
-    let topic = document.getElementById("topic-input").value;
-    let url = 'http://127.0.0.1:5000'
-    try{
-        //fetches joke from flask server
-        let response = await fetch(url);
-        let data = await response.json();
-        //updates joke container with new joke
-        document.getElementById('joke-container').innerText = data.joke;
-        document.getElementById('error-container').innerText = '';
-    } catch(error){
-        //displays error
-        document.getElementById('error-container').innerText = 'Something went wrong bruh';
-        document.getElementById('joke-container').innerText = '';
-    }
+// async function generateJoke(){
+//     let topic = document.getElementById("topic-input").value;
+//     let url = 'http://127.0.0.1:5000'
+//     try{
+//         //fetches joke from flask server
+//         let response = await fetch(url);
+//         let data = await response.json();
+//         //updates joke container with new joke
+//         document.getElementById('joke-container').innerText = data.joke;
+//         document.getElementById('error-container').innerText = '';
+//     } catch(error){
+//         //displays error
+//         document.getElementById('error-container').innerText = 'Something went wrong bruh';
+//         document.getElementById('joke-container').innerText = '';
+//     }
 
+// }
+
+function test_function() {
+    $.ajax({
+        type: "POST",
+        url: "/generate_joke",
+        data: null, //content of the text box
+        success: function (response) {
+            console.log(response);
+        }
+    })
 }
